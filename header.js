83cd31a5f95d536d54d53859f8cae2f8f18447d8
Cthomp7 <cthomp7@saic.edu>
2022-09-29 11:04:55 -0500

    Added Header Effect!

---
+++ header.js
@@ -0,0 +1,27 @@
+const navItems = document.querySelectorAll('.navigation ul a');
+
+function init(){
+    let star = `<img src="images/header-star.png">`
+}
+
+navItems.addEventListener("mouseover", function(){
+    //I need my image to be absolute, but the target element to be relative to the <a> element 
+    //create div element and then with that newly created div let's append that star img in there.
+});
+
+
+
+
+
+
+
+
+
+
+window.onload = function() {init();};
+
+//let menuItems = document.querySelectorAll('.')
+
+// $('.navigation ul a').on("mouseover", function(){
+//     $(this).append()
+// })
