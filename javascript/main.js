1cb8ae9647cb0d43b715eef8ca051a31252e866a
Cthomp7 <cthomp7@saic.edu>
2022-10-20 10:45:35 -0500

    fixed header

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,20 @@
+let clicked = false;
+
+$(window).resize(function() {
+    console.log("triggered");
+    if ($('body').width() >= 500) {
+        $('.nav-controller').css("display", "block");
+    } else {
+        $('.nav-controller').css("display", "none");
+    }
+});
+
 $('.hamburger').click(function(){
-    $('.nav-controller').toggle();
+    if (clicked == false) {
+        $('.nav-controller').css("display", "block");
+        clicked = true;
+    } else {
+        $('.nav-controller').css("display", "none");
+        clicked = false;
+    }
 });
