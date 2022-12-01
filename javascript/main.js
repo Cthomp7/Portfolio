9450b433315edb2f6cc25a92e1cb74b13c48c009
Cthomp7 <cthomp7@saic.edu>
2022-11-30 18:15:03 -0600

    fixed main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,26 +1,16 @@
-// let jQuery = document.createElement("script");
-// jQuery.src = "https://code.jquery.com/jquery-3.6.1.js";
-// jQuery.integrity = "sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI="
-// jQuery.crossOrigin = "anonymous";
-// document.head.append(jQuery);
-
 let doc = document.location.pathname;
 console.log(doc);
 
 $(document).ready(function() {
-    $('head').append('<link rel="stylesheet" href="Portfolio/css/fonts.css"/>');
-    $('head').append('<link rel="stylesheet" href="Portfolio/css/site.css"/>');
-    $('header').load('Portfolio/header.html');
-    $('head').append('<link rel="stylesheet" href="Portfolio/css/header.css"/>');
+    $('head').append(`<link rel="stylesheet" href="/css/fonts.css"/>`);
+    $('head').append(`<link rel="stylesheet" href="/css/site.css"/>`);
+    $('header').load(`/header.html`);
+    $('head').append(`<link rel="stylesheet" href="/css/header.css"/>`);
 
-    //********** FIX ************ 
-    //if statements are triggered correctly
-    //css rules are not being applied
     if (doc.includes("about") == true) {
         $('#about').addClass('target');
         let about = document.getElementById("about");
         about.style.color = "rgb(200, 200, 200) !important;";
-        //something fucked up with the id's on the li elements
         console.log("You are in the about section");
     } else if (doc.includes("portfolio") == true || doc.includes("projects") == true) {
         $('#portfolio').css({'color': 'rgb(200, 200, 200)'});
